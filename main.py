30c26034b912d1eed5179559aad1546a1c3b9588
djheidihoe mbp <djheidihoe@gmail.com>
2024-06-10 13:42:41 +0200

    went through final challenge

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    # print(text)
+    chars_dict = get_chars_dict(text)
+    sorted_chars_dict = dict(
+        sorted(chars_dict.items(), key=lambda item: item[1], reverse=True)
+    )
+    # print(chars_dict)
+    num_words = get_num_words(text)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print_dict(sorted_chars_dict)
+    print("--- End report ---")
+    # print(chars_dict)
 
-    def count_words():
-        words = text.split()
-        # print(words)
-        # print(len(words))
-        chars = dict()
-        for word in words:
-            for char in word.lower():
-                if char in chars:
-                    chars[char] += 1
-                else:
-                    chars[char] = 1
-        # return len(words)
-        print(chars)
 
-    # print(count_words())
-    count_words()
+def print_dict(dict: dict) -> None:
+    for k, v in dict.items():
+        if not k.isalpha():
+            continue
+        print(f"The {k} character was found {v} times")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
 def get_book_text(path):
--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
+    # num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     print(chars_dict)
 
