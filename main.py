23758423fe6a6159ee7dc12b92ec14ae8edd0dd9
djheidihoe mbp <djheidihoe@gmail.com>
2025-05-25 23:29:36 +0200

    status before refactoring for CLI arguments

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_words, get_book_text, get_chars_dict
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -7,10 +10,13 @@ def main():
     )
     # print(chars_dict)
     num_words = get_num_words(text)
-    print(f"--- Begin report of {book_path} ---")
-    print(f"{num_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
     print_dict(sorted_chars_dict)
-    print("--- End report ---")
+    print("============= END ===============")
     # print(chars_dict)
 
 
@@ -18,28 +24,8 @@ def print_dict(dict: dict) -> None:
     for k, v in dict.items():
         if not k.isalpha():
             continue
-        print(f"The {k} character was found {v} times")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
-
-def get_chars_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
-    return chars
-
-
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+        # print(f"The {k} character was found {v} times")
+        print(f"{k}: {v}")
 
 
 main()
